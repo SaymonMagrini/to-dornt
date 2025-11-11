f751ee5e5201115e90c8a3f6d0f1f73db7a081b1
Joelinto Siqueira <joelintoSiqueira@gmail.com>
2025-11-10 22:04:03 -0300

    outro

--- public/register.php
+++ public/register.php
@@ -2,20 +2,13 @@
 require_once __DIR__ . '/../vendor/autoload.php';
 
 use App\Controllers\AuthController;
-use Symfony\Component\HttpFoundation\Request;
 
-$request = Request::createFromGlobals();
+
 $controller = new AuthController();
 
-if ($request->getMethod() === 'POST') {
-    $name = $request->request->get('name');
-    $email = $request->request->get('email');
-    $password = $request->request->get('password');
 
-    $controller->auth->register($name, $email, $password);
-    header('Location: /login.php');
-    exit;
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $controller->register();
 } else {
-    $html = (new App\Core\View())->render('auth/register', ['csrf' => App\Core\Csrf::token()]);
-    echo $html;
+    $controller->showRegister();
 }
