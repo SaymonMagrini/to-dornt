66405129160458a28e8cb15bb75d7c92af0e37f1
Joelinto Siqueira <joelintoSiqueira@gmail.com>
2025-11-11 20:44:41 -0300

    e pra funcionar espero

--- public/logout.php
+++ public/logout.php
@@ -4,5 +4,4 @@ require_once __DIR__ . '/../vendor/autoload.php';
 use App\Controllers\AuthController;
 
 $controller = new AuthController();
-$response = $controller->logout();
-$response->send();
+$controller->logout();
