ffb8a5e3b7940a432d15ee9ab6bb74624e13fd2c
Joelinto Siqueira <joelintoSiqueira@gmail.com>
2025-11-10 21:28:23 -0300

    ajustes en flash

--- app/Core/Flash.php
+++ app/Core/Flash.php
@@ -1,19 +1,29 @@
 <?php
 
 namespace App\Core;
+
 class Flash
 {
     public static function push(string $type, string $message): void
     {
-        Csrf::ensureSession();
-        $_SESSION['_flash'][] = ['type' => $type, 'message' => $message];
+        if (!isset($_SESSION)) {
+            session_start();
+        }
+
+        $_SESSION['flash'][] = [
+            'type' => $type,
+            'message' => $message
+        ];
     }
 
-    public static function pullAll(): array
+    public static function getAll(): array
     {
-        Csrf::ensureSession();
-        $all = $_SESSION['_flash'] ?? [];
-        unset($_SESSION['_flash']);
-        return $all;
+        if (!isset($_SESSION)) {
+            session_start();
+        }
+
+        $messages = $_SESSION['flash'] ?? [];
+        unset($_SESSION['flash']);
+        return $messages;
     }
 }
