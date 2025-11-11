22d5f88c7e0efe2acdecd722dc0a55ad644026d0
SaymonMagrini <saymomagrini@gmail.com>
2025-11-10 21:04:09 -0300

    db migrates release

--- db/migrations/20251020000101_create_tags_table.php
+++ db/migrations/20251020000101_create_tags_table.php
@@ -3,14 +3,13 @@ declare(strict_types=1);
 
 use Phinx\Migration\AbstractMigration;
 
-final class CreateCategoriesTable extends AbstractMigration
+final class CreateTagsTable extends AbstractMigration
 {
     public function change(): void
     {
-        $this->table('categories')
+        $this->table('tags')
             ->addColumn('user_id', 'integer', ['signed' => false, 'null' => true])
             ->addColumn('name', 'string', ['limit' => 32])
-            ->addColumn('description', 'string', ['limit' => 256])
             ->addForeignKey('user_id', 'users', 'id', ['delete' => 'CASCADE'])
             ->create();
     }
